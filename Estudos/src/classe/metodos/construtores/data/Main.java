7af69f96cd87e7322dce384133d28c23eab703cb
Ferdinando Rainert <nandorainert2004@gmail.com>
2026-09-25 14:05:22 -0300

    Desafio com construtores data

--- Estudos/src/classe/metodos/construtores/data/Main.java
+++ Estudos/src/classe/metodos/construtores/data/Main.java
@@ -1,9 +1,20 @@
 package classe.metodos.construtores.data;
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Data d1 = new Data();
+		
+		Data d2 = new Data(31, 12, 2020);
+		
+		String dataFormatada1 = d1.obterData();
+		
+		//Mostrando a data
+		System.out.println(dataFormatada1);
+		System.out.printf(d2.obterData());
 
 	}
 
